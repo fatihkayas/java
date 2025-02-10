cea464706c18be6c9662bdbf65b53157a8761ea6
fatihkayas <seavsdeep@gmail.com>
2025-02-10 11:13:53 +0100

    Add first codeline

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World 2!");
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name + "!");
+        scan.close();
+    }
+}
